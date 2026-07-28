12a78084d1cadeb9e7993f401d8bc50a7122ba7f
Victor <victorgmaciel@protonmail.com>
2026-07-28 15:06:29 -0700

    adding PizzaOfTheDay component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { React, StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <div>
         <h1>Padre Ginos - Order Now</h1>
         <Order />
+        <PizzaOfTheDay />
       </div>
     </StrictMode>
   );
