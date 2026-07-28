1d0d9ac4277831af8c9e3a9989071310c71ae525
Victor <victorgmaciel@protonmail.com>
2026-07-28 14:41:11 -0700

    adding </StrictMode> to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { React, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 
 const App = () => {
   return (
-    <div>
-      <h1>Padre Ginos - Order Now</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Padre Ginos - Order Now</h1>
+        <Order />
+      </div>
+    </StrictMode>
   );
 };
 
