9141fc8e93608e3408aa6accafe6f650baca67df
Victor <victorgmaciel@protonmail.com>
2026-08-04 10:27:54 -0700

    adding header and context

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
+import Header from "./Header";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 className="logo">Padre Ginos - Order Now</h1>
+        <Header />
         <Order />
         <PizzaOfTheDay />
       </div>
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav>
+      <h1 className="logo">Padre Gino's Pizza</h1>
+      <div className="nav-cart">
+        🛒<span className="nav-cart-number">5</span>
+      </div>
+    </nav>
+  );
+}
