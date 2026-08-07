0d5120195633866b18ff4c54012ff6c1658abfba
Victor <victorgmaciel@protonmail.com>
2026-08-06 18:21:57 -0700

    foundation for testing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { environmentManager } from "@tanstack/react-query";
 
 export default defineConfig({
   server: {
@@ -16,4 +17,7 @@ export default defineConfig({
     },
   },
   plugins: [tanstackRouter(), react()],
+  test: {
+    environment: "happy-dom",
+  },
 });
