5f81b0600bc744e2ed6b6ad5c7e397014bbe64fa
Victor <victorgmaciel@protonmail.com>
2026-08-10 13:37:11 -0700

    added testing libaries and basic tests

--- src/__tests__/Pizza.test.jsx
+++ src/__tests__/Pizza.test.jsx
@@ -1,2 +1,25 @@
-import { render } from "@testing-library/react";
-import { render, }
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
+import Pizza from "../Pizza";
+
+afterEach(cleanup);
+
+test("alt test renders on Pizza image", async () => {
+  const name = "My Favorita Pizza";
+  const src = "https://picsum.photos/200";
+  const screen = render(
+    <Pizza name={name} description="super cool pizza" img={src} />,
+  );
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
+
+test("to have default image if none if provided", async () => {
+  const screen = render(
+    <Pizza name="something else" description="super cool pizza" />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBe("");
+});
